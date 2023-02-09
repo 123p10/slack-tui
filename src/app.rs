88304016a88744354a2441ce462533ba8b2db67b
Owen <obrake3@gmail.com>
2023-02-08 21:45:04 -0800

    Improve UI

--- src/app.rs
+++ src/app.rs
@@ -1,20 +1,81 @@
 use std::io::{Result};
 
 pub struct ProfileInfo {
-    pub name: String
+    pub name: String,
+    token: String,
 }
+
+pub struct Message {
+    pub author: String,
+    pub text: Option<String>,
+    pub media: Option<String>,
+    pub timestamp: u64
+}
+
+pub struct Channel {
+    pub name: String,
+    pub messages: Vec<Message> 
+}
+
+pub struct Space {
+    pub name: String,
+    pub channels: Vec<Channel>
+}
+
 pub struct App {
     pub running: bool,
-    pub profile: ProfileInfo
+    pub connected: bool,
+    pub profile: Option<ProfileInfo>,
+    pub current_space_index: Option<usize>,
+    pub spaces: Vec<Space>
+}
+
+const DEBUG: bool = false;
+
+pub fn init_test_data() -> App
+{
+    App {
+        running: true,
+        connected: true,
+        profile: Some(ProfileInfo {
+            name: String::from("Test Name"),
+            token: String::from("Test Token")
+        }),
+        spaces: vec![ 
+            Space {
+                name: String::from("Space 1"),
+                channels: vec![
+                    Channel {
+                        name: String::from("Channel 1"),
+                        messages: Vec::new()
+                    },
+                    Channel {
+                        name: String::from("Channel 2"),
+                        messages: Vec::new()
+                    }
+                ] 
+            }
+        ],
+        current_space_index: Some(0),
+    }
+}
+
+pub fn init_real_data() -> App
+{
+    App {
+        running: true,
+        connected: false,
+        spaces: Vec::new(),
+        current_space_index: None,
+        profile: None
+    }
 }
 
 pub fn init() -> App
 {
-    App {
-        running: true,
-        profile: ProfileInfo {
-            name: String::from("Test Name")
-        }
+    match DEBUG {
+        true => init_test_data(),
+        false => init_real_data()
     }
 }
 
--- src/terminal.rs
+++ src/terminal.rs
@@ -4,7 +4,7 @@ use tui::{
     backend::{CrosstermBackend},
     Terminal,
     layout::{Constraint, Direction, Layout, Rect},
-    widgets::{Block, Borders, Paragraph},
+    widgets::{Block, Borders, Paragraph, List, ListItem},
     text::Text,
     Frame
 };
@@ -16,6 +16,8 @@ use crossterm::{
 use crate::app;
 
 const TIMEOUT_MS: u64 = 250;
+const MAX_SPACE_NAME: usize = 25;
+const MAX_CHANNEL_NAME: usize = 25;
 
 pub fn setup_tui() -> Result<Terminal<CrosstermBackend<std::io::Stdout>>>
 {
@@ -58,6 +60,23 @@ pub fn tick(terminal: &mut Terminal<CrosstermBackend<std::io::Stdout>>, app_inst
 
 fn ui(f: &mut Frame<CrosstermBackend<std::io::Stdout>>, app_inst: &mut app::App)
 {
+    match app_inst.connected
+    {
+        true => render_connected(f, app_inst),
+        false => render_disconnected(f, app_inst)
+    }
+}
+
+fn render_disconnected(f: &mut Frame<CrosstermBackend<std::io::Stdout>>, app_inst: &mut app::App)
+{
+    
+    let block = Block::default().borders(Borders::ALL).title("slack-tui");
+    let txt = Paragraph::new("Disconnected").block(block);
+    f.render_widget(txt, f.size());
+}
+
+fn render_connected(f: &mut Frame<CrosstermBackend<std::io::Stdout>>, app_inst: &mut app::App)
+{ 
    let chunks = Layout::default()
         .direction(Direction::Vertical)
         .constraints(
@@ -69,7 +88,7 @@ fn ui(f: &mut Frame<CrosstermBackend<std::io::Stdout>>, app_inst: &mut app::App)
         .split(f.size());
     render_tabs(f, chunks[0], app_inst);
     render_main_space(f, chunks[1], app_inst);
-}
+} 
 
 fn render_tabs(f: &mut Frame<CrosstermBackend<std::io::Stdout>>, area: Rect, app_inst: &mut app::App)
 {
@@ -96,16 +115,57 @@ fn render_space_info(f: &mut Frame<CrosstermBackend<std::io::Stdout>>, area: Rec
     let space_info = Block::default().title("Space Info").borders(Borders::ALL);
     f.render_widget(space_info, area);
 }
+
 fn render_spaces(f: &mut Frame<CrosstermBackend<std::io::Stdout>>, area: Rect, app_inst: &mut app::App)
 {
-    let spaces = Block::default().title("Spaces").borders(Borders::ALL);
-    f.render_widget(spaces, area);
+    let block = Block::default().title("Spaces").borders(Borders::ALL);
+    let mut list_items = Vec::with_capacity(app_inst.spaces.len());
+    for space in app_inst.spaces.iter() {
+        if space.name.len() <= MAX_SPACE_NAME
+        {
+            let name = String::from(&space.name);
+            let item = ListItem::new(name);
+            list_items.push(item);
+        }
+    }
+    let list = List::new(list_items).block(block);
+    f.render_widget(list, area);
 }
 fn render_profile(f: &mut Frame<CrosstermBackend<std::io::Stdout>>, area: Rect, app_inst: &mut app::App)
 {
     let block = Block::default().title("Profile").borders(Borders::ALL);
-    let txt = Paragraph::new(app_inst.profile.name.clone()).block(block);
-    f.render_widget(txt, area);
+    match &app_inst.profile
+    {
+        Some(profile) => {    
+            let txt = Paragraph::new(profile.name.clone()).block(block);
+            f.render_widget(txt, area);
+        }
+        None => {
+            f.render_widget(block, area);
+        }
+    }
+}
+
+fn render_channel_list(f: &mut Frame<CrosstermBackend<std::io::Stdout>>, area: Rect, app_inst: &mut app::App)
+{ 
+    let block = Block::default().title("Channels").borders(Borders::ALL);
+    let space_index = app_inst.current_space_index;
+    match space_index {
+        Some(idx) => {
+            let mut list_items = Vec::with_capacity(app_inst.spaces[idx].channels.len());
+            for channel in app_inst.spaces[idx].channels.iter() {
+                if channel.name.len() <= MAX_CHANNEL_NAME
+                {
+//                    let name = String::from(&channel.name);
+                    let item = ListItem::new(channel.name.clone());
+                    list_items.push(item);
+                }
+            }
+            let list = List::new(list_items).block(block).highlight_symbol(">>");
+            f.render_widget(list, area);
+        },
+        None => ()
+    }
 }
 
 fn render_main_space(f: &mut Frame<CrosstermBackend<std::io::Stdout>>, area: Rect, app_inst: &mut app::App)
@@ -119,8 +179,7 @@ fn render_main_space(f: &mut Frame<CrosstermBackend<std::io::Stdout>>, area: Rec
             ].as_ref()
         )
         .split(area);
-   let l_block = Block::default().title("Channels").borders(Borders::ALL);
-   f.render_widget(l_block, chunks[0]);
+   render_channel_list(f, chunks[0], app_inst);
    let m_block = Block::default().title("Text").borders(Borders::ALL);
    f.render_widget(m_block, chunks[1]);
 }
